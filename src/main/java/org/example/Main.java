3759e96ac22901c45eb5385250fe7d43160c6b5b
AnatolyBarinov <62007944+AnatolyBarinov@users.noreply.github.com>
2023-11-27 15:45:37 +0300

    Код с лекции

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.example.service.PostService;
+
+public class Main {
+  public static void main(String[] args) {
+    final var factory = new DefaultListableBeanFactory();
+    final var reader = new XmlBeanDefinitionReader(factory);
+    reader.loadBeanDefinitions("beans.xml");
+
+    // получаем по имени бина
+    final var controller = factory.getBean("postController");
+
+    // получаем по классу бина
+    final var service = factory.getBean(PostService.class);
+
+    // по умолчанию создаётся лишь один объект на BeanDefinition
+    final var isSame = service == factory.getBean("postService");
+  }
+}
